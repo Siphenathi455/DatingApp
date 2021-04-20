024aec9e99b70569dfd906b29f3cbb1cd34f741e
Siphenathi455 <siphenathiyandisa3@gmail.com>
2021-04-20 03:19:52 +0200

    End of section 6

--- client1/src/app/app.module.ts
+++ client1/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
-import {FormsModule } from '@angular/forms';
+import {FormsModule, ɵInternalFormsSharedModule } from '@angular/forms';
 import {NavComponent } from './nav/nav.component';
 import {BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
@@ -12,6 +12,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
+
+
 
 
 
@@ -20,14 +28,19 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
